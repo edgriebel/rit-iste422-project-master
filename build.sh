d1c85e8348678102e383c4846e6dcf6ae8e4e69a
Lucas Kohorst <kohorstlucas@gmail.com>
2020-02-06 18:57:37 -0500

    Added mkdir

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+mkdir build
 javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar -d build src/java/*.java
 javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:build -d build test/java/*.java
 java -cp .:lib/junit-4.12.jar:build:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
-java -cp build RunEdgeConvert
+java -cp build RunEdgeConvert 
