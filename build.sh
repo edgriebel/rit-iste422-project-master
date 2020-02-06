43575c8f0f6a42c13763bba334d24f19cfd986c2
Nicholas Feldman <ncf1362@rit.edu>
2020-02-06 14:28:22 -0500

    Organize source files into directories
    Create src/java and test/java, and move source files in.
    Modify buildscript to use these new folders, and output to a build
    directory.

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
-java RunEdgeConvert
+
+SRC_DIR="src/java"
+TEST_DIR="test/java"
+BUILD_DIR="build"
+
+
+javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar \
+	$SRC_DIR/*.java $TEST_DIR/*.java \
+	-d $BUILD_DIR
+
+java -cp $BUILD_DIR:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar \
+	org.junit.runner.JUnitCore \
+	EdgeConnectorTest
+
+java -cp $BUILD_DIR \
+	RunEdgeConvert
+
