da64847f7944081cf1b7ccc5272734686a9b9d5b
bdc5435 <bdc5435@rit.edu>
2020-02-09 01:54:07 -0500

    Moved files to proper folders and changed build file to reflect the changes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
-java RunEdgeConvert
+javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar -d build/ src/java/*.java
+javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:build/ -d build/ test/java/*.java
+java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:build/ org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp build/ RunEdgeConvert
