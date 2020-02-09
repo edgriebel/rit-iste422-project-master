b1f7e1aa82947e47e810934f89424fdb3ba0a743
zach <zpe4421@g.rit.edu>
2020-02-08 19:43:41 -0500

    Made some small changes to file structure.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
-java RunEdgeConvert
+javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:/src/java/*.java
+java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:/test/java/EdgeConnectorTest
+java /src/java/runEdgeConvert
