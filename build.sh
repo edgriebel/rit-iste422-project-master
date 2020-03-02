7d9d4ff75f69f7c331dbeeebcf659664de4e44e9
BrenBren <btj9560@rit.edu>
2020-03-02 17:50:38 -0500

    restructured project for gradle

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
-java RunEdgeConvert
